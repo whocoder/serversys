aff2440615fc812a905bfa7d5f6880a5616cd1e1
who codes <cam@strafeodyssey.com>
2015-09-01 00:42:19 -0400

    Improve ads SQL structure

--- addons/sourcemod/configs/serversys/sql-init-scripts/adverts.sql
+++ addons/sourcemod/configs/serversys/sql-init-scripts/adverts.sql
@@ -2,8 +2,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `adverts` (
   `advertid` int(11) NOT NULL,
-  `text` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `sid` int(11) NOT NULL
+  `sid` int(11) NOT NULL,
+  `text` varchar(128) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `adverts`
