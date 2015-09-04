fb79f899cfff60099b8081c1f2dd6157d18723a0
who codes <cam@strafeodyssey.com>
2015-09-04 05:42:23 -0400

    Improve SQL + Add map playtimes

--- addons/sourcemod/configs/serversys/sql-init-scripts/init.sql
+++ addons/sourcemod/configs/serversys/sql-init-scripts/init.sql
@@ -6,30 +6,45 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `ip` varchar(64) NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `maps` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `lastplayed` bigint(20) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `pid` int(11) NOT NULL,
   `auth` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `lastseen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `lastseen` bigint(20) NOT NULL DEFAULT 0
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `playtime` (
   `row` int(11) NOT NULL,
   `pid` int(11) NOT NULL,
   `sid` int(11) NOT NULL,
-  `time` int(32) NOT NULL
+  `time` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `maps_playtime` (
+  `row` int(11) NOT NULL,
+  `mid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 ALTER TABLE `servers`
-  ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`), ADD KEY `name` (`name`), ADD INDEX(`id`), ADD INDEX(`name`);
+  ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`), ADD KEY `name` (`name`), ADD KEY `ip` (`ip`), ADD INDEX(`id`), ADD INDEX(`name`), ADD INDEX(`ip`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`pid`), ADD KEY `pid` (`pid`), ADD KEY `auth` (`auth`), ADD KEY `name` (`name`), ADD KEY `lastseen` (`lastseen`), ADD INDEX(`pid`), ADD INDEX(`auth`), ADD INDEX(`name`), ADD INDEX(`lastseen`);
 
 ALTER TABLE `playtime`
   ADD PRIMARY KEY (`row`), ADD KEY `row` (`row`), ADD KEY `pid` (`pid`), ADD KEY `sid` (`sid`), ADD KEY `time` (`time`), ADD INDEX(`row`), ADD INDEX(`pid`), ADD INDEX(`sid`), ADD INDEX(`time`);
+
+ALTER TABLE `maps_playtime`
+  ADD PRIMARY KEY (`row`), ADD KEY `row` (`row`), ADD KEY `mid` (`mid`), ADD KEY `sid` (`sid`), ADD KEY `time` (`time`), ADD INDEX(`row`), ADD INDEX(`mid`), ADD INDEX(`sid`), ADD INDEX(`time`);
 
 
 ALTER TABLE `servers`
@@ -39,4 +54,7 @@ ALTER TABLE `users`
   MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `playtime`
+  MODIFY `row` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `maps_playtime`
   MODIFY `row` int(11) NOT NULL AUTO_INCREMENT;
